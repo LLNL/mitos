43078d99598f83c8bd48099d680493934d00da18
Alfredo Gimenez <gimenez1@llnl.gov>
2015-04-13 21:39:26 -0700

    better matmul

--- src/examples/matmul.cpp
+++ src/examples/matmul.cpp
@@ -1,10 +1,13 @@
+#include <cstdlib>
 #include <iostream>
 
 #include <omp.h>
 
+#define ROW_MAJOR(x,y,width) y*width+x
+
 void workit()
 {
-    int N = 4096;
+    int N = 2048;
 
     double *a;
     double *b;
@@ -17,21 +20,30 @@ void workit()
     c = new double[N*N];
 
     for(i=0; i<N; ++i)
+    {
         for(j=0; j<N; ++j)
-            c[i*N+j] = 0;
+        {
+            a[ROW_MAJOR(i,j,N)] = (float)rand();
+            b[ROW_MAJOR(i,j,N)] = (float)rand();
+            c[ROW_MAJOR(i,j,N)] = 0;
+        }
+    }
 
-#pragma omp parallel for private(i,j,k,N) shared(a,b,c)
+#pragma omp parallel for 
     for(i=0; i<N; ++i)
+    {
         for(j=0; j<N; ++j)
+        {
             for(k=0; k<N; ++k)
             {
-                int cidx = i*N+j;
-                int aidx = i*N+k;
-                int bidx = k*N+j;
-                double av = a[aidx];
-                double bv = a[aidx];
-                c[cidx] += av*bv;
+                c[ROW_MAJOR(i,j,N)] += a[ROW_MAJOR(i,k,N)]*b[ROW_MAJOR(k,j,N)];
             }
+        }
+    }
+
+    int randx = N*((float)rand() / (float)RAND_MAX);
+    int randy = N*((float)rand() / (float)RAND_MAX);
+    std::cout << c[randx*N+randy] << std::endl;
 
 }
 
