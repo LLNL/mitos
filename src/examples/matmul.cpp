8702c98ca95c7b9e624033d02f138abf169db4bb
Alfredo Gimenez <gimenez1@llnl.gov>
2015-04-14 12:55:54 -0700

    fixed examples

--- src/examples/matmul.cpp
+++ src/examples/matmul.cpp
@@ -5,49 +5,48 @@
 
 #define ROW_MAJOR(x,y,width) y*width+x
 
-void workit()
+void init_matrices(int N, double **a, double **b, double **c)
 {
-    int N = 2048;
-
-    double *a;
-    double *b;
-    double *c;
-
     int i,j,k;
 
-    a = new double[N*N];
-    b = new double[N*N];
-    c = new double[N*N];
+    *a = new double[N*N];
+    *b = new double[N*N];
+    *c = new double[N*N];
 
     for(i=0; i<N; ++i)
     {
         for(j=0; j<N; ++j)
         {
-            a[ROW_MAJOR(i,j,N)] = (float)rand();
-            b[ROW_MAJOR(i,j,N)] = (float)rand();
-            c[ROW_MAJOR(i,j,N)] = 0;
+            (*a)[ROW_MAJOR(i,j,N)] = (double)rand();
+            (*b)[ROW_MAJOR(i,j,N)] = (double)rand();
+            (*c)[ROW_MAJOR(i,j,N)] = 0;
         }
     }
+}
 
-#pragma omp parallel for 
-    for(i=0; i<N; ++i)
+void matmul(int N, double *a, double *b, double *c)
+{
+#pragma omp parallel for
+    for(int i=0; i<N; ++i)
     {
-        for(j=0; j<N; ++j)
+        for(int j=0; j<N; ++j)
         {
-            for(k=0; k<N; ++k)
+            for(int k=0; k<N; ++k)
             {
                 c[ROW_MAJOR(i,j,N)] += a[ROW_MAJOR(i,k,N)]*b[ROW_MAJOR(k,j,N)];
             }
         }
     }
 
-    int randx = N*((float)rand() / (float)RAND_MAX);
-    int randy = N*((float)rand() / (float)RAND_MAX);
-    std::cout << c[randx*N+randy] << std::endl;
-
+    int randx = N*((float)rand() / (float)RAND_MAX+1);
+    int randy = N*((float)rand() / (float)RAND_MAX+1);
+    std::cout << c[ROW_MAJOR(randx,randy,N)] << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    workit();
+    int N = (argc == 2) ? atoi(argv[1]) : 1024;
+    double *a,*b,*c;
+    init_matrices(N,&a,&b,&c);
+    matmul(N,a,b,c);
 }
--- src/examples/test.cpp
+++ src/examples/test.cpp
@@ -1,87 +1,90 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
 
-#include "Mitos.h"
-
-std::vector<perf_event_sample> samples;
-
-char* cmd;
-
-void dump()
-{
-    // Header
-    std::cout << "variable,xidx,yidx,ip,time,latency,dataSource,hit,address,cpu" << std::endl;
-
-    // Tuples
-    for(size_t i=0; i<samples.size(); i++)
-    {
-        std::cout << samples[i].data_symbol;
-        std::cout << ",";
-        std::cout << std::dec << Mitos_x_index(&samples[i]);
-        std::cout << ",";
-        std::cout << std::dec << Mitos_y_index(&samples[i]);
-        std::cout << ",";
-        std::cout << std::hex << samples[i].ip ;
-        std::cout << ",";
-        std::cout << std::hex << samples[i].time ;
-        std::cout << ",";
-        std::cout << std::dec << samples[i].weight ;
-        std::cout << ",";
-        std::cout << std::hex << Mitos_data_source(&samples[i]) ;
-        std::cout << ",";
-        std::cout << std::hex << Mitos_hit_type(&samples[i]) ;
-        std::cout << ",";
-        std::cout << std::hex << samples[i].addr ;
-        std::cout << ",";
-        std::cout << std::dec << samples[i].cpu << std::endl;
-        std::cout << std::endl;
-    }
-}
+#include <Mitos.h>
 
 void sample_handler(perf_event_sample *sample, void *args)
 {
     Mitos_resolve_symbol(sample);
-    samples.push_back(*sample);
+
+    std::cout << sample->data_symbol;
+    std::cout << ",";
+    std::cout << std::dec << Mitos_x_index(sample);
+    std::cout << ",";
+    std::cout << std::dec << Mitos_y_index(sample);
+    std::cout << ",";
+    std::cout << std::hex << sample->ip ;
+    std::cout << ",";
+    std::cout << std::hex << sample->time ;
+    std::cout << ",";
+    std::cout << std::dec << sample->weight ;
+    std::cout << ",";
+    std::cout << std::hex << Mitos_data_source(sample);
+    std::cout << ",";
+    std::cout << std::hex << Mitos_hit_type(sample);
+    std::cout << ",";
+    std::cout << std::hex << sample->addr ;
+    std::cout << ",";
+    std::cout << std::dec << sample->cpu << std::endl;
+    std::cout << std::endl;
 }
 
-void workit()
+#define ROW_MAJOR(x,y,width) y*width+x
+
+void init_matrices(int N, double **a, double **b, double **c)
 {
-    int N = 1024;
-
-    double *a;
-    double *b;
-    double *c;
-
     int i,j,k;
 
-    a = (double*)malloc(sizeof(double)*N*N);
-    b = (double*)malloc(sizeof(double)*N*N);
-    c = (double*)malloc(sizeof(double)*N*N);
+    *a = new double[N*N];
+    *b = new double[N*N];
+    *c = new double[N*N];
 
-    size_t dims[2];
-    dims[0] = N;
-    dims[1] = N;
-
-    Mitos_add_symbol("a",a,sizeof(double),dims,2);
-    Mitos_add_symbol("b",b,sizeof(double),dims,2);
-    Mitos_add_symbol("c",c,sizeof(double),dims,2);
+    size_t dims[2] = {N,N};
+    Mitos_add_symbol("a",*a,sizeof(double),dims,2);
+    Mitos_add_symbol("b",*b,sizeof(double),dims,2);
+    Mitos_add_symbol("c",*c,sizeof(double),dims,2);
 
     for(i=0; i<N; ++i)
+    {
         for(j=0; j<N; ++j)
-            c[i*N+j] = 0;
+        {
+            (*a)[ROW_MAJOR(i,j,N)] = (double)rand();
+            (*b)[ROW_MAJOR(i,j,N)] = (double)rand();
+            (*c)[ROW_MAJOR(i,j,N)] = 0;
+        }
+    }
+}
 
-#pragma omp parallel for private(i,j,k,N) shared(a,b,c)
-    for(i=0; i<N; ++i)
-        for(j=0; j<N; ++j)
-            for(k=0; k<N; ++k)
-                c[i*N+j] += a[i*N+k] * b[k*N+j];
+void matmul(int N, double *a, double *b, double *c)
+{
+#pragma omp parallel for
+    for(int i=0; i<N; ++i)
+    {
+        for(int j=0; j<N; ++j)
+        {
+            for(int k=0; k<N; ++k)
+            {
+                c[ROW_MAJOR(i,j,N)] += a[ROW_MAJOR(i,k,N)]*b[ROW_MAJOR(k,j,N)];
+            }
+        }
+    }
 
+    int randx = N*((float)rand() / (float)RAND_MAX+1);
+    int randy = N*((float)rand() / (float)RAND_MAX+1);
+    std::cout << c[ROW_MAJOR(randx,randy,N)] << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    cmd = argv[0];
+    // Print header
+    std::cout << "variable,xidx,yidx,ip,time,latency,dataSource,hit,address,cpu" << std::endl;
+
+    // Initialize memory
+    int N = (argc == 2) ? atoi(argv[1]) : 1024;
+    double *a,*b,*c;
+    init_matrices(N,&a,&b,&c);
 
     Mitos_set_sample_mode(SMPL_MEMORY);
     Mitos_set_handler_fn(&sample_handler,NULL);
@@ -89,8 +92,6 @@ int main(int argc, char **argv)
     Mitos_prepare(0);
 
     Mitos_begin_sampler();
-    workit();
+    matmul(N,a,b,c);
     Mitos_end_sampler();
-
-    dump();
 }
